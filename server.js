6954bfe439792703bab1df75e197c820db2cc752
Yazan Deek <ydeek91@yahoo.com>
2020-10-02 18:06:20 -0400

    delete a note.

--- server.js
+++ server.js
@@ -65,3 +65,33 @@ app.post("/api/notes", function (req, res) {
         console.error(err);
     }
 });
+
+// Delete a note
+
+app.delete("/api/notes/:id", function (req, res) {
+    try {
+        //  reads the json file
+        notesData = fs.readFileSync("./Develop/db/db.json", "utf8");
+        // parse the data to get an array of the objects
+        notesData = JSON.parse(notesData);
+        // delete the old note from the array on note objects
+        notesData = notesData.filter(function (note) {
+            return note.id != req.params.id;
+        });
+        // make it string(stringify)so you can write it to the file
+        notesData = JSON.stringify(notesData);
+        // write the new notes to the file
+        fs.writeFile("./Develop/db/db.json", notesData, "utf8", function (err) {
+            // error handling
+            if (err) throw err;
+        });
+
+        // change it back to an array of objects & send it back to the browser (client)
+        res.send(JSON.parse(notesData));
+
+        // error handling
+    } catch (err) {
+        throw err;
+        console.log(err);
+    }
+});
